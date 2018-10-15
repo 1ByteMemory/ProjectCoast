5ba953dd9b3cfd9587e213848df5e93a76fa8a3f
Dan <dan.jones8902@gmail.com>
2018-10-15 15:09:16 +0100

    Player Movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    
+    // The speed of the Player
+    public float speed = 1.0f;
+
+
+    // Player Movement
+    void Update () {
+
+        if ((Input.GetKey("a") || Input.GetKey(KeyCode.LeftArrow)) && transform.position.x > -8.4f)
+        {
+            transform.position += new Vector3(-speed * Time.deltaTime, 0, 0);
+        }
+
+        if ((Input.GetKey("d") || Input.GetKey(KeyCode.RightArrow)) && transform.position.x < 8.4f)
+        {
+            transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+        }
+    }
+}
