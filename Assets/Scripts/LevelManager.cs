9dcb65788e252dae285a5b4ba781b977aaa6bfe0
Dan <dan.jones8902@gmail.com>
2018-10-18 13:30:43 +0100

    Executable programme in App.

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+    private Rigidbody[] rb;
+
+	// At the start of the level this block will run.
+	void Start () {
+        rb = GetComponentsInChildren<Rigidbody>();
+        
+        // Calls wait Function
+        StartCoroutine("Wait");
+    }
+	
+    // Wait function.
+    IEnumerator Wait()
+    {
+        // Cycles through all the ingriedients to cascade them.
+        for (int i = 0; i < rb.Length; i++)
+        {
+            // Enables Gravity for one ingreidient.
+            rb[i].useGravity = true;
+            yield return new WaitForSeconds(2);
+        }
+
+        
+    }
+}
